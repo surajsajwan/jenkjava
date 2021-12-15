78c94245a6cf0640dda16ca29015fedb78ea2c55
suraj <ssajwan28@gmail.com>
2021-12-15 11:30:44 +0530

    Added java proj to github

---
+++ bms/src/com/greycampus/App.java
@@ -0,0 +1,9 @@
+package com.greycampus;
+
+public class App {
+
+	public static void main(String[] args) {
+      System.out.println("HELLO WELCOME TO JAVA PROJECT");
+	}
+
+}
---
+++ bms/src/module-info.java
@@ -0,0 +1,2 @@
+module bms {
+}
